e8d5715d167181112fea76606c93e64673992e78
Gustavo <gusgoludo9@gmail.com>
2022-05-20 15:36:27 -0500

    Se implemento primeNG

--- spotify/src/app/app.module.ts
+++ spotify/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SideBarComponent } from './Components/side-bar/side-bar.component';
 import { DashboardComponent } from './Components/dashboard/dashboard.component';
+import { PrimeNgModule } from './prime-ng/prime-ng.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { DashboardComponent } from './Components/dashboard/dashboard.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PrimeNgModule
   ],
   providers: [],
   bootstrap: [AppComponent]
